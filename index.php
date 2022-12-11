3829de0604b276bbf57d51d4eb1c0d56b94519ce
dikiogres <dikiogres@gmail.com>
2022-12-11 12:22:44 +0700

    finish

--- index.php
+++ index.php
@@ -1,21 +1,33 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Pendaftaran Siswa Baru | SMK Coding</title>
+    <title>New Student Registration Form | SMAN 1 Coding</title>
 </head>
 
 <body>
     <header>
-        <h3>Pendaftaran Siswa Baru</h3>
-        <h1>SMK Coding</h1>
+        <h3>New Student Registration Form</h3>
+        <h1>SMAN 1 Coding</h1>
     </header>
 
     <h4>Menu</h4>
     <nav>
         <ul>
-            <li><a href="form-daftar.php">Daftar Baru</a></li>
-            <li><a href="list-siswa.php">Pendaftar</a></li>
+            <li><a href="registration-form.php">Register New</a></li>
+            <li><a href="student-list.php">Registrants</a></li>
         </ul>
     </nav>
+    <?php if(isset($_GET['status'])): ?>
+    <p>
+        <?php
+            if($_GET['status'] == 'success'){
+                echo "New student registration successful!";
+            } else {
+                echo "Registration failed!";
+            }
+        ?>
+    </p>
+<?php endif; ?>
+
     </body>
 </html>
